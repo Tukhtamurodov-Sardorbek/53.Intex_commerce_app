057a4a2504c4b2cc20acc77a3e6fd7b1790adfb7
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-08-21 17:19:41 +0500

    🛡 Code Optimized 🛡

--- lib/pages/no_internet_connection_page/no_internet_controller.dart
+++ lib/pages/no_internet_connection_page/no_internet_controller.dart
@@ -1,7 +1,13 @@
 import 'package:get/get.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:intex_commerce/data/dio_client.dart';
+import 'package:intex_commerce/core/app_services/environment_service.dart';
 import 'package:intex_commerce/pages/splash_page/splash_controller.dart';
+import 'package:intex_commerce/routes/app_routes.dart';
 
+import '../../core/app_services/database_service.dart';
+import '../../data/models/category_model.dart';
+import '../../data/models/products_model.dart';
 
 class NoInternetConnectionController extends GetxController {
   bool _isLoading = false;
@@ -26,6 +32,38 @@ class NoInternetConnectionController extends GetxController {
       Get.find<SplashController>().fetchData(false);
     }
   }
+
+  // Future<String> getCategoryList() async {
+  //   await DioService().GET(
+  //       api: Environment.envVariable('apiGetCategory'),
+  //       params: DioService().paramsEmpty()).then((value) => {
+  //         parseCategory(value),
+  //   });
+  //   return '';
+  // }
+  //
+  // void parseCategory(String? response) {
+  //   if (response != null) {
+  //     CaterogyModel caterogy = caterogyListModelFromJson(response);
+  //     StorageService.to.setData(StorageKeys.categoryList, caterogy.data);
+  //   }
+  // }
+  //
+  // Future<String> getProductModel() async {
+  //   await DioService().GET(
+  //       api: Environment.envVariable('apiGetProduct'),
+  //       params: DioService().paramsEmpty()).then(
+  //           (value) => {parseProduct(value)},
+  //   );
+  //   return '';
+  // }
+  //
+  // void parseProduct(String? response) {
+  //   if (response != null) {
+  //     ProductModel product = productModelFromJson(response);
+  //     StorageService.to.setData(StorageKeys.productList, product.data);
+  //   }
+  // }
 
   Future<void> onPressed() async {
     isLoading = true;
