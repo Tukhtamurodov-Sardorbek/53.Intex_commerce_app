071adf72cb9f4c155ae4c7bc6ada58e75bc0cf87
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-08-21 17:19:58 +0500

    🛡 Code Optimized 🛡

--- lib/pages/splash_page/splash_controller.dart
+++ lib/pages/splash_page/splash_controller.dart
@@ -1,10 +1,13 @@
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:intex_commerce/data/dio_client.dart';
 import 'package:intex_commerce/core/app_services/database_service.dart';
 import 'package:intex_commerce/core/app_services/environment_service.dart';
 import 'package:intex_commerce/core/app_services/log_service.dart';
-import 'package:intex_commerce/data/models/category_list_model.dart';
+import 'package:intex_commerce/data/models/category_model.dart';
+import 'package:intex_commerce/data/models/info_model.dart';
 import 'package:intex_commerce/data/models/products_model.dart';
+import 'package:intex_commerce/pages/home_page/home_controller.dart';
 import 'package:intex_commerce/routes/app_routes.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
@@ -18,15 +21,15 @@ class SplashController extends GetxController {
   Future<void> fetchData(bool isRefresh) async {
     bool hasInternet = await InternetConnectionChecker().hasConnection;
     if (hasInternet) {
-      await Future.wait([getCategoryList(), getProductList()]).then((value){
-        if (StorageService.to.checkData(StorageKeys.categoryList) && StorageService.to.checkData(StorageKeys.productList)) {
+      await Future.wait([getCategoryList(), getProductList(), getInfo()]).then((value){
+        if (StorageService.to.checkData(StorageKeys.categoryList) && StorageService.to.checkData(StorageKeys.productList) && StorageService.to.checkData(StorageKeys.info)) {
           Get.offNamed(AppRoutes.home);
         } else {
           Get.offNamed(AppRoutes.noInternet);
         }
       });
     } else {
-      if (StorageService.to.checkData(StorageKeys.categoryList) && StorageService.to.checkData(StorageKeys.productList)) {
+      if (StorageService.to.checkData(StorageKeys.categoryList) && StorageService.to.checkData(StorageKeys.productList) && StorageService.to.checkData(StorageKeys.info)) {
         Get.offNamed(AppRoutes.home);
       } else {
         Get.offNamed(AppRoutes.noInternet);
@@ -35,18 +38,16 @@ class SplashController extends GetxController {
   }
 
   Future<String> getCategoryList() async {
-    Log.i('GET CATEGORY LIST');
     await DioService().GET(
       api: Environment.envVariable('apiGetCategory'),
       params: DioService().paramsEmpty(),
-    ).then((value) => {parseCategory(value)});
+    ).then((value) => parseCategory(value));
     return '';
   }
-
   void parseCategory(String? response) {
     if (response != null) {
-      Log.i('PARSE CATEGORY LIST');
       CaterogyModel category = caterogyListModelFromJson(response);
+      Log.i('SAVING CATEGORY: ${category.string()}');
       StorageService.to.setData(StorageKeys.categoryList, category.data);
     }
   }
@@ -55,14 +56,29 @@ class SplashController extends GetxController {
     await DioService().GET(
       api: Environment.envVariable('apiGetProduct'),
       params: DioService().paramsEmpty(),
-    ).then((value) => {parseProduct(value)});
+    ).then((value) => parseProduct(value));
     return '';
   }
   void parseProduct(String? response) {
     if (response != null) {
       ProductModel product = productModelFromJson(response);
-      Log.i('SAVING PRODUCTS: ${product.data}');
+      Log.i('SAVING PRODUCTS: ${product.string()}');
       StorageService.to.setData(StorageKeys.productList, product.data);
+    }
+  }
+
+  Future<String> getInfo() async {
+    await DioService().GET(
+      api: Environment.envVariable('apiGetInfo'),
+      params: DioService().paramsEmpty(),
+    ).then((value) => parseInfo(value));
+    return '';
+  }
+  void parseInfo(String? response) {
+    if (response != null) {
+      InfoModel info = infoModelFromJson(response);
+      Log.i('SAVING INFO: ${info.data.first.toString()}');
+      StorageService.to.setData(StorageKeys.info, info.data.first);
     }
   }
 }
