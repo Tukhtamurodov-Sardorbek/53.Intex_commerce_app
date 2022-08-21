ce87c34b33c0bc3f4681391bfd80c511fc7795bb
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-08-21 17:19:12 +0500

    🛡 Code Optimized 🛡

--- lib/pages/home_page/home_controller.dart
+++ lib/pages/home_page/home_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,9 +10,9 @@ import 'package:intex_commerce/data/dio_client.dart';
 import 'package:intex_commerce/core/app_services/database_service.dart';
 import 'package:intex_commerce/core/app_services/environment_service.dart';
 import 'package:intex_commerce/core/app_services/log_service.dart';
-import 'package:intex_commerce/data/models/category_list_model.dart';
+import 'package:intex_commerce/data/models/category_model.dart';
+import 'package:intex_commerce/data/models/info_model.dart';
 import 'package:intex_commerce/data/models/products_model.dart';
-import 'package:intex_commerce/data/models/sites_model.dart';
 import 'package:intex_commerce/pages/home_page/home_ui.dart';
 import 'package:intex_commerce/pages/home_page/widgets/dialog_widgets.dart';
 import 'package:intex_commerce/pages/splash_page/splash_controller.dart';
@@ -34,13 +35,21 @@ class HomeController extends GetxController {
 
   List<Map<int, List<Product>>> _products = [];
   DateTime _lastPressed = DateTime(0);
+  Info _info = Info(
+      phoneNumber: '+998(99)535-53-33',
+      addressRu: 'Проспект Мустакиллик\n59А 100000 Узбекистан,\nТашкент',
+      addressUz: 'Mustaqillik shoh ko\'chasi\n59А 100000 O\'zbekiston,\nToshkent',
+      workTimeRu: 'Будние дни: 10:00 - 22:00\nБез выходных',
+      workTimeUz: 'Ish kunlari: 10:00 - 22:00\nDam olish kunlarisiz',
+      telegramLink: 'https://t.me/basseinintexuzb',
+      instagramLink: 'https://www.instagram.com/intexshop_uz/',
+  );
   List<Category> _categories = [];
   bool _displayShadow = false;
   String _language = 'ru';
   String _dioException = '';
   bool _isPostingConsultation = false;
   bool _isPostingOrder = false;
-  List<SitesModelData> sitesModelDataList = [];
 
   final _textBasseyn = <String>[
     'strength',
@@ -82,7 +91,6 @@ class HomeController extends GetxController {
 
   @override
   void onInit() {
-    apiGetSites();
     readData();
     super.onInit();
   }
@@ -137,22 +145,21 @@ class HomeController extends GetxController {
   //   const phoneNumber = '+998907997720';
   //   const url = 'tel:$phoneNumber';
   // }
-
   Future<void> _launchUrl(String url, bool isPhoneCall) async {
     if(isPhoneCall){
-      if (await canLaunchUrl(Uri.parse(url))) {
-        await launchUrl(Uri.parse(url));
+      if (await canLaunch('tel:$url')) {
+        await launch('tel:$url');
       }
     }else{
-      if (await canLaunchUrl(Uri.parse(url))) {
-        await launchUrl(Uri.parse(url),);
+      if (await canLaunch(url)) {
+        await launch(url, forceSafariVC: false);
       }
     }
   }
 
-  Future<void> launchPhoneCall() => _launchUrl(Environment.envVariable('phone'), true);
-  Future<void> launchTelegram() => _launchUrl(Environment.envVariable('telegram'), false);
-  Future<void> launchInstagram() => _launchUrl(Environment.envVariable('instagram'), false);
+  Future<void> launchPhoneCall() => _launchUrl(getPhoneCall(), true);
+  Future<void> launchTelegram() => _launchUrl(_info.telegramLink, false);
+  Future<void> launchInstagram() => _launchUrl(_info.instagramLink, false);
 
   /// Pull to refresh
   Future<void> handleRefresh() async {
@@ -185,11 +192,6 @@ class HomeController extends GetxController {
         ? _products[superIndex][categoryId]![index].frameUz
         : _products[superIndex][categoryId]![index].frameRu;
   }
-  String getProductCategoryName({required int superIndex, required int categoryId, required int index}) {
-    return _language == 'uz'
-        ? _products[superIndex][categoryId]![index].categoryNameUz
-        : _products[superIndex][categoryId]![index].categoryNameRu;
-  }
   String getCategoryName({required int superIndex}) {
     return _language == 'uz'
         ? _categories[superIndex].nameUz
@@ -207,6 +209,26 @@ class HomeController extends GetxController {
       default:
         return AppColors.transparent;
     }
+  }
+
+  String getPhoneCall(){
+    String phoneToCall = _info.phoneNumber.replaceAll(RegExp(r'[^\+\d]+'),'');
+    if(phoneToCall.startsWith('998')){
+      phoneToCall = '+$phoneToCall';
+      update();
+    }
+    return phoneToCall;
+  }
+  String getPhone() => _info.phoneNumber;
+  String getAddress() => _language == 'uz' ? _info.addressUz : _info.addressRu;
+  String getWorkTime() {
+    String workTime = '';
+    if(_language == 'uz'){
+      workTime = _info.workTimeUz.replaceFirst('. ', '\n');
+    }else{
+      workTime = _info.workTimeRu.replaceFirst('. ', '\n');
+    }
+    return workTime;
   }
 
   /// Text Controllers
@@ -232,12 +254,25 @@ class HomeController extends GetxController {
 
   readData() async {
     List<Product> productList = [];
-    if (StorageService.to.getData(StorageKeys.productList) is List<Product>) {
+
+    if(StorageService.to.getData(StorageKeys.info) is Info){
+     _info = StorageService.to.getData(StorageKeys.info);
+    }else{
+      _info = Info.fromJson(StorageService.to.getData(StorageKeys.info));
+    }
+
+    if (StorageService.to.getData(StorageKeys.categoryList) is List<Category>) {
       _categories = StorageService.to.getData(StorageKeys.categoryList);
+    } else {
+      if (StorageService.to.checkData(StorageKeys.categoryList)){
+        _categories = List<Category>.from(StorageService.to.getData(StorageKeys.categoryList).map((x) => Category.fromJson(x)));
+      }
+    }
+
+    if (StorageService.to.getData(StorageKeys.productList) is List<Product>) {
       productList = StorageService.to.getData(StorageKeys.productList);
     } else {
       if (StorageService.to.checkData(StorageKeys.categoryList) && StorageService.to.checkData(StorageKeys.productList)){
-        _categories = List<Category>.from(StorageService.to.getData(StorageKeys.categoryList).map((x) => Category.fromJson(x)));
         productList = List<Product>.from(StorageService.to.getData(StorageKeys.productList).map((x) => Product.fromJson(x)));
       }
     }
@@ -250,50 +285,12 @@ class HomeController extends GetxController {
       });
     }
     update();
+    Log.i('READ INFO $_info');
     Log.i('READ CATEGORIES: $_categories');
     Log.i('READ PRODUCTS $_products');
   }
 
   /// Network
-  void consultationResponse({required String? response, required BuildContext context}) {
-    if (response != null) {
-      _isPostingConsultation = false;
-      update();
-      Get.back();
-      openDialog(
-        context: context,
-        child: const SuccessDialog(),
-      );
-      clearConsultTextControllers();
-    }
-  }
-  void orderResponse({required String? response, required BuildContext context}) {
-    Log.wtf(response ?? 'NO ORDER RESPONSE');
-
-    if(response != null && response == 'not_available'){
-      _isPostingOrder = false;
-      update();
-      Get.back();
-      openDialog(
-        context: context,
-        child: const OutOfStockDialog(),
-      );
-      clearTextControllers();
-      handleRefresh();
-    }
-    else if (response != null && response != 'not_available') {
-      _isPostingOrder = false;
-      update();
-      Get.back();
-      openDialog(
-        context: context,
-        child: const SuccessDialog(),
-      );
-      clearTextControllers();
-    }
-  }
-
-
   Future<void> postConsultation(BuildContext context) async {
     String name = consultNameController.text.trim().toString();
     String phone = consultPhoneController.text.trim().toString();
@@ -326,7 +323,19 @@ class HomeController extends GetxController {
       Timer(const Duration(seconds: 3), () => Get.back());
     }
   }
-  Future<void> postOrder(BuildContext context, int productId) async {
+  void consultationResponse({required String? response, required BuildContext context}) {
+    if (response != null) {
+      _isPostingConsultation = false;
+      update();
+      Get.back();
+      openDialog(
+        context: context,
+        child: const SuccessDialog(),
+      );
+      clearConsultTextControllers();
+    }
+  }
+  Future<void> postOrder(BuildContext context, String productId) async {
     String name = nameController.text.trim().toString();
     String phone = phoneController.text.trim().toString();
     String address = addressController.text.trim().toString();
@@ -364,6 +373,31 @@ class HomeController extends GetxController {
       Timer(const Duration(seconds: 3), () => Get.back());
     }
   }
+  void orderResponse({required String? response, required BuildContext context}) {
+    Log.wtf(response ?? 'NO ORDER RESPONSE');
+
+    if(response != null && response == 'not_available'){
+      _isPostingOrder = false;
+      update();
+      Get.back();
+      openDialog(
+        context: context,
+        child: const OutOfStockDialog(),
+      );
+      clearTextControllers();
+      handleRefresh();
+    }
+    else if (response != null && response != 'not_available') {
+      _isPostingOrder = false;
+      update();
+      Get.back();
+      openDialog(
+        context: context,
+        child: const SuccessDialog(),
+      );
+      clearTextControllers();
+    }
+  }
 
 
   /// Localization
@@ -371,17 +405,5 @@ class HomeController extends GetxController {
     _language = _language == 'uz' ? 'ru' : 'uz';
     update();
     AppTranslations.changeLocale(_language);
-  }
-
-  void apiGetSites() {
-    DioService().GET(api: Environment.envVariable('apiGetSites'), params: DioService().paramsEmpty()).then((value) => {
-     parseSites(value),
-    });
-  }
-  void parseSites(String? response) {
-    if(response != null){
-      SitesModel sitesModel = sitesModelFromJson(response);
-      sitesModelDataList = sitesModel.data;
-    }
   }
 }
